039134d43dc202cfbf11473d5ae80c57a0b31527
Jordan Peck <jordan.me2@gmail.com>
2022-06-12 14:56:41 +0100

    Remove old FastSIMD

--- src/FastSIMD/FastSIMD_Level_SSE41.cpp
+++
@@ -1,7 +0,0 @@
-#include "FastSIMD/FastSIMD.h"
-
-#if FASTSIMD_COMPILE_SSE41
-#include "Internal/SSE.h"
-#define FS_SIMD_CLASS FastSIMD::SSE41
-#include "Internal/SourceBuilder.inl"
-#endif
--- src/FastSIMD/FastSIMD_Level_SSE42.cpp
+++
@@ -1,7 +0,0 @@
-#include "FastSIMD/FastSIMD.h"
-
-#if FASTSIMD_COMPILE_SSE42
-#include "Internal/SSE.h"
-#define FS_SIMD_CLASS FastSIMD::SSE42
-#include "Internal/SourceBuilder.inl"
-#endif
