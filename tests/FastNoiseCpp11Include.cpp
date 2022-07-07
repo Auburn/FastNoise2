283038be2a3f454d6c5ee30c6fb7ac88aaf4a343
Evgeny Gorodetsky <evsgor@gmail.com>
2022-07-07 23:57:51 +0300

    Add multi-threaded example to reproduce the defect #90

--- tests/FastNoiseCpp11Include.cpp
+++ tests/FastNoiseCpp11Include.cpp
@@ -1,10 +1,15 @@
 #include <FastNoise/FastNoise.h>
 #include <FastNoise/Metadata.h>
 
+#include <taskflow/taskflow.hpp>
+
+#include <array>
 #include <iostream>
 
 int main()
 {
+#if 0
+
     auto node = FastNoise::New<FastNoise::FractalFBm>();
 
     std::cout << node->GetSIMDLevel() << std::endl;
@@ -13,7 +18,6 @@ int main()
     node->SetGain( FastNoise::New<FastNoise::Value>() );
 
     const int size = 4;
-
     float noise[size * size];
 
     node->GenUniformGrid2D( noise, 0, 0, size, size, 0.02f, 1337 );
@@ -24,6 +28,34 @@ int main()
     }
 
     std::cout << std::endl;
+
+#else
+
+    const int size = 16;
+
+    // Multi-threaded use-case
+    std::array<std::array<float, size * size>, 16U> noise_per_thread;
+    tf::Taskflow task_flow;
+    task_flow.for_each_index(0U, 64U, 1U,
+                              [&noise_per_thread, size]( const uint32_t t ) {
+                                auto node = FastNoise::New<FastNoise::FractalFBm>();
+                                node->SetSource( FastNoise::New<FastNoise::Simplex>() );
+                                node->SetGain( FastNoise::New<FastNoise::Value>() );
+                                node->GenUniformGrid2D( noise_per_thread[t].data(), 0, 0, size, size, 0.02f, 1337 );
+                            });
+    tf::Executor().run( task_flow ).get();
+
+    for( int t = 0; t < noise_per_thread.size(); ++t )
+    {
+        std::cout << std::endl << "Thread " << t << " noise: ";
+        for( float value: noise_per_thread[t] )
+        {
+            std::cout << value << ", ";
+        }
+    }
+        
+
+#endif
 
     // SmartNode down cast example
 #if !FASTNOISE_USE_SHARED_PTR
