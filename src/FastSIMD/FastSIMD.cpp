420029d021e89b9367d0614b2b061686c35b7295
Auburn <jordan.me2@gmail.com>
2023-04-15 13:03:51 +0100

    Avoid race condition in CPUMaxSIMDLevel()

--- src/FastSIMD/FastSIMD.cpp
+++ src/FastSIMD/FastSIMD.cpp
@@ -79,106 +79,101 @@ static int64_t xgetbv( int ctr )
 }
 #endif
 
-FASTSIMD_API FastSIMD::eLevel FastSIMD::CPUMaxSIMDLevel()
+static FastSIMD::eLevel GetCPUMaxSIMDLevel()
 {
-    static eLevel simdLevel = Level_Null;
-
-    if ( simdLevel > Level_Null )
-    {
-        return simdLevel;
-    }
+    FastSIMD::eLevel simdLevel = FastSIMD::Level_Null;
 
 #if FASTSIMD_x86
-    int abcd[4] = { 0,0,0,0 }; // cpuid results
+    int abcd[4] = { 0, 0, 0, 0 }; // cpuid results
 
 #if !FASTSIMD_64BIT
     simdLevel = Level_Scalar; // default value
 
     cpuid( abcd, 0 ); // call cpuid function 0
-    if ( abcd[0] == 0 )
+    if( abcd[0] == 0 )
         return simdLevel; // no further cpuid function supported
 
     cpuid( abcd, 1 ); // call cpuid function 1 for feature flags
-    if ( (abcd[3] & (1 << 0)) == 0 )
+    if( ( abcd[3] & ( 1 << 0 ) ) == 0 )
         return simdLevel; // no floating point
-    if ( (abcd[3] & (1 << 23)) == 0 )
+    if( ( abcd[3] & ( 1 << 23 ) ) == 0 )
         return simdLevel; // no MMX
-    if ( (abcd[3] & (1 << 15)) == 0 )
+    if( ( abcd[3] & ( 1 << 15 ) ) == 0 )
         return simdLevel; // no conditional move
-    if ( (abcd[3] & (1 << 24)) == 0 )
+    if( ( abcd[3] & ( 1 << 24 ) ) == 0 )
         return simdLevel; // no FXSAVE
-    if ( (abcd[3] & (1 << 25)) == 0 )
+    if( ( abcd[3] & ( 1 << 25 ) ) == 0 )
         return simdLevel; // no SSE
     simdLevel = Level_SSE;
     // 1: SSE supported
 
-    if ( (abcd[3] & (1 << 26)) == 0 )
+    if( ( abcd[3] & ( 1 << 26 ) ) == 0 )
         return simdLevel; // no SSE2
 #else
     cpuid( abcd, 1 ); // call cpuid function 1 for feature flags
 #endif
 
-    simdLevel = Level_SSE2; // default value for 64bit
+    simdLevel = FastSIMD::Level_SSE2; // default value for 64bit
     // 2: SSE2 supported
 
-    if ( (abcd[2] & (1 << 0)) == 0 )
+    if( ( abcd[2] & ( 1 << 0 ) ) == 0 )
         return simdLevel; // no SSE3
-    simdLevel = Level_SSE3;
+    simdLevel = FastSIMD::Level_SSE3;
     // 3: SSE3 supported
 
-    if ( (abcd[2] & (1 << 9)) == 0 )
+    if( ( abcd[2] & ( 1 << 9 ) ) == 0 )
         return simdLevel; // no SSSE3
-    simdLevel = Level_SSSE3;
+    simdLevel = FastSIMD::Level_SSSE3;
     // 4: SSSE3 supported
 
-    if ( (abcd[2] & (1 << 19)) == 0 )
+    if( ( abcd[2] & ( 1 << 19 ) ) == 0 )
         return simdLevel; // no SSE4.1
-    simdLevel = Level_SSE41;
+    simdLevel = FastSIMD::Level_SSE41;
     // 5: SSE4.1 supported
 
-    if ( (abcd[2] & (1 << 23)) == 0 )
+    if( ( abcd[2] & ( 1 << 23 ) ) == 0 )
         return simdLevel; // no POPCNT
-    if ( (abcd[2] & (1 << 20)) == 0 )
+    if( ( abcd[2] & ( 1 << 20 ) ) == 0 )
         return simdLevel; // no SSE4.2
-    simdLevel = Level_SSE42;
+    simdLevel = FastSIMD::Level_SSE42;
     // 6: SSE4.2 supported
 
-    if ( (abcd[2] & (1 << 26)) == 0 )
+    if( ( abcd[2] & ( 1 << 26 ) ) == 0 )
         return simdLevel; // no XSAVE
-    if ( (abcd[2] & (1 << 27)) == 0 )
+    if( ( abcd[2] & ( 1 << 27 ) ) == 0 )
         return simdLevel; // no OSXSAVE
-    if ( (abcd[2] & (1 << 28)) == 0 )
+    if( ( abcd[2] & ( 1 << 28 ) ) == 0 )
         return simdLevel; // no AVX
 
     uint64_t osbv = xgetbv( 0 );
-    if ( (osbv & 6) != 6 )
+    if( ( osbv & 6 ) != 6 )
         return simdLevel; // AVX not enabled in O.S.
-    simdLevel = Level_AVX;
+    simdLevel = FastSIMD::Level_AVX;
     // 7: AVX supported
 
     cpuid( abcd, 7 ); // call cpuid leaf 7 for feature flags
-    if ( (abcd[1] & (1 << 5)) == 0 )
+    if( ( abcd[1] & ( 1 << 5 ) ) == 0 )
         return simdLevel; // no AVX2
-    simdLevel = Level_AVX2;
+    simdLevel = FastSIMD::Level_AVX2;
     // 8: AVX2 supported
 
-    if( (osbv & (0xE0)) != 0xE0 )
+    if( ( osbv & ( 0xE0 ) ) != 0xE0 )
         return simdLevel; // AVX512 not enabled in O.S.
-    if ( (abcd[1] & (1 << 16)) == 0 )
+    if( ( abcd[1] & ( 1 << 16 ) ) == 0 )
         return simdLevel; // no AVX512
     cpuid( abcd, 0xD ); // call cpuid leaf 0xD for feature flags
-    if ( (abcd[0] & 0x60) != 0x60 )
+    if( ( abcd[0] & 0x60 ) != 0x60 )
         return simdLevel; // no AVX512
     // 9: AVX512 supported
 
     cpuid( abcd, 7 ); // call cpuid leaf 7 for feature flags
-    if ( (abcd[1] & (1 << 31)) == 0 )
+    if( ( abcd[1] & ( 1 << 31 ) ) == 0 )
         return simdLevel; // no AVX512VL
     // 10: AVX512VL supported
 
-    if ( (abcd[1] & 0x40020000) != 0x40020000 )
+    if( ( abcd[1] & 0x40020000 ) != 0x40020000 )
         return simdLevel; // no AVX512BW, AVX512DQ
-    simdLevel = Level_AVX512;
+    simdLevel = FastSIMD::Level_AVX512;
     // 11: AVX512BW & AVX512DQ supported
 #endif
 
@@ -186,6 +181,12 @@ FASTSIMD_API FastSIMD::eLevel FastSIMD::CPUMaxSIMDLevel()
     simdLevel = Level_NEON;
 #endif
 
+    return simdLevel;
+}
+
+FASTSIMD_API FastSIMD::eLevel FastSIMD::CPUMaxSIMDLevel()
+{
+    static eLevel simdLevel = GetCPUMaxSIMDLevel();
     return simdLevel;
 }
 
